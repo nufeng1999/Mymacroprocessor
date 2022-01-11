f8a60dce17eecbc1b2777b5e97a42acd87556bb0
nufeng1999 <18478162@21cn.com>
2022-01-11 17:22:34 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
-from setuptools import setup 
-setup(name='Mymacroprocessor',
-      version='0.0.1',
-      description='python file macro processor',
-      author='nufeng',
-      author_email='18478162@qq.com',
-      requires= ['re'],
-      url='https://github.com/nufeng1999/Mymacroprocessor/',
-      download_url='https://github.com/nufeng1999/Mymacroprocessor/tarball/0.0.1',
-      packages=['Mymacroprocessor'],
-      keywords=['python', 'macro', 'processor', 'if','ifdef','ifndef','elif','else','endif','defined','define','undef'],
-      license="apache 3.0",
-      include_package_data=True
-      )
+#!/usr/bin/env python
+# coding: utf-8
+with open("README.md", "r") as f:
+	long_description = f.read()
+import setuptools
+setuptools.setup(name='Mymacroprocessor',
+    version='0.0.2',
+    description='python file macro processor',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='nufeng',
+    author_email='18478162@qq.com',
+    requires= ['re'],
+    url='https://github.com/nufeng1999/Mymacroprocessor/',
+    download_url='https://github.com/nufeng1999/Mymacroprocessor/releases/tag/0.0.1',
+    packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+    keywords=['python', 'macro', 'processor', 'if','ifdef','ifndef','elif','else','endif','defined','define','undef'],
+    license="apache 3.0",
+    install_requires=[],
+    include_package_data=True
+    )
